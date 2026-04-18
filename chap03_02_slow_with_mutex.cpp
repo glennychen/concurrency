c867cc300828f3ad25525cb2d1a3aae8209b4b3c
Glenn Y Chen <glenn.chen01@gmail.com>
2026-04-17 21:44:56 -0400

    Add multithreaded increment function with mutex

---
+++ chap03_02_slow_with_mutex.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <numeric>
+using namespace std;
+
+mutex mtx;
+void increment_n_times(long long& total, size_t n){
+    for(size_t i=0;i<n;++i){
+        lock_guard<mutex> lk(mtx);
+        ++total;
+    }
+}
+
+int main()
+{
+    long long total=0;
+    {
+        vector<jthread> v;
+        int n=1000000;
+        for(int i=0;i<4;++i){
+            v.emplace_back(increment_n_times, ref(total), n);
+        }
+    }
+    cout << total;
+    return 0;
+}
